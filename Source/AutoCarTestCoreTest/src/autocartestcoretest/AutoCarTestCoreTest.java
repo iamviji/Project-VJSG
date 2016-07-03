0d59afe5eb3515ef8eb9fcc285b3b2d6f215e8c9
Vijaya Kumar <ViKumar@infinera.com>
2016-07-03 19:56:02 +0530

    Few more files are added

--- Source/AutoCarTestCoreTest/src/autocartestcoretest/AutoCarTestCoreTest.java
+++ Source/AutoCarTestCoreTest/src/autocartestcoretest/AutoCarTestCoreTest.java
@@ -5,6 +5,7 @@
  */
 package autocartestcoretest;
 import AutoCarTestCore.*;
+import java.io.*;
 /**
  *
  * @author vikumar
@@ -14,10 +15,22 @@ public class AutoCarTestCoreTest {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException 
+    {
         // TODO code application logic here
         SampleClass s = new SampleClass ();
         s.print ();
+        FileInputStream in = null;
+        
+        try 
+        {
+            String filename = args [0];
+            in = new FileInputStream (filename);
+        } 
+        finally 
+        {
+            
+        }
     }
     
 }
