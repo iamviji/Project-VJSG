d75f6f55e988801354cda0bc69157e152eb938e8
iamviji <i.am.viji@gmail.com>
2016-07-14 00:21:11 +0530

    web app set-up file

---
+++ Source/AutoCarWebApp/setup-django.bat
@@ -0,0 +1,11 @@
+START python get-pip.py
+START pip install virtualenv
+START python -m pip install --upgrade pip
+START pip install virtualenvwrapper
+START mkvirtualenv autorto
+START autorto
+START pip install Django
+
+
+
+
